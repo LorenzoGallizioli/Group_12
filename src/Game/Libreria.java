5accb080ed1103f189da80c682ffade266a064cf
LorenzoGallizioli <l.gallizioli1@studenti.unibg.it>
2023-05-06 18:26:05 +0200

    Cambia da enum a Color

--- src/Game/Libreria.java
+++ src/Game/Libreria.java
@@ -44,5 +44,4 @@ public class Libreria {
 		return libreria;
 	}
 	
-	
 }
--- src/Game/Tessera.java
+++ src/Game/Tessera.java
@@ -1,5 +1,7 @@
 package Game;
 
+import java.awt.Color;
+
 /**
  * Definisce le tessere da inserire nella libreria.
  * 
@@ -7,8 +9,7 @@ package Game;
  */
 public class Tessera {
 	
-	protected Colore colore;
-	protected Oggetto oggetto;
+	protected Color colore;
 	
 	/**
 	 * Il costruttore della classe.
@@ -16,28 +17,8 @@ public class Tessera {
 	 * @param colore
 	 * 	 Il colore della tessera.
 	 */
-	public Tessera(Colore colore) {
-		this.colore = colore;
-		switch(colore) {
-			case VERDE:
-				this.oggetto = Oggetto.GATTI;
-				break;
-			case GIALLO:
-				this.oggetto = Oggetto.GIOCHI;
-				break;
-			case BLU:
-				this.oggetto = Oggetto.CORNICI;
-				break;
-			case BIANCO:
-				this.oggetto = Oggetto.LIBRI;
-				break;
-			case CIANO:
-				this.oggetto = Oggetto.TROFEI;
-				break;
-			case MAGENTA:
-				this.oggetto = Oggetto.PIANTE;
-				break;
-		}
+	public Tessera(Color colore) {
+		this.colore = colore;	
 	}
 
 	/**
@@ -46,18 +27,8 @@ public class Tessera {
 	 * @return Color
 	 * 	 Il colore della tessera.
 	 */
-	public Colore getColore() {
+	public Color getColore() {
 		return colore;
-	}
-
-	/**
-	 * Setta il colore della tessera.
-	 * 
-	 * @param colore
-	 * 	 Il colore da settare.
-	 */
-	public void setColore(Colore colore) {
-		this.colore = colore;
 	}
 
 }
