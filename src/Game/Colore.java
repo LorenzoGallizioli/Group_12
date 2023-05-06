17fb24f534e1aa622e6f1705b86861b29cf602ca
LorenzoGallizioli <l.gallizioli1@studenti.unibg.it>
2023-05-06 12:37:49 +0200

    implementa classi libreria, tessera e colore

---
+++ src/Game/Colore.java
@@ -0,0 +1,15 @@
+package Game;
+
+/**
+ * Definisce i colori delle tessere.
+ *
+ * @author lorenzogallizioli
+ */
+public enum Colore {
+	VERDE, 
+	GIALLO, 
+	BLU, 
+	BIANCO, 
+	CIANO, 
+	MAGENTA;	
+}
---
+++ src/Game/Libreria.java
@@ -0,0 +1,48 @@
+package Game;
+
+/**
+ * Definisce le librerie (una per ciascun giocatore) da riempire con le tessere prese dalla Tavola.
+ * 
+ * @author lorenzogallizioli
+ */
+public class Libreria {
+	
+	protected Tessera libreria[][] = new Tessera[5][5];
+	
+	/**
+	 * Il costruttore della classe.
+	 */
+	public Libreria() {
+		for(int i=0; i<=4; i++) {
+			for(int j=0; j<=4; j=j+1) {
+		        libreria[i][j]=null;
+		    }
+		}
+	}
+
+	/**
+	 * Inserisce la tessera selezionata all'interno di uno specifico spazio della libreria.
+	 * 
+	 * @param tessera
+	 *	 La tessera selezionata.
+	 * @param riga
+	 * 	 La riga della matrice.
+	 * @param colonna
+	 * 	 La colonna della matrice.
+	 */
+	public void inserisciTessera(Tessera tessera, int riga, int colonna) {
+		libreria[riga][colonna] = tessera;
+	}
+
+	/**
+	 * Restituisce la libreria.
+	 * 
+	 * @return libreria
+	 * 	 La libreria
+	 */
+	public Tessera[][] getLibreria() {
+		return libreria;
+	}
+	
+	
+}
