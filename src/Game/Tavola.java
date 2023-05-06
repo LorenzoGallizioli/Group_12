137d03be42d568e378f1bd374db6aa48a7ec749d
Davide <d.dellanno@studenti.unibg.it>
2023-05-06 14:19:40 +0200

    implementazione tavola da gioco con tessere colorate(vrs. prova) + pannello grafico per contenere la matrice

--- src/Game/Tavola.java
+++ src/Game/Tavola.java
@@ -3,10 +3,10 @@ package Game;
 public class Tavola {
 	
 	static int  numPlayers = 0;
-	public Tessera[][] tavolaDaGioco = new Tessera[9][9];
 	
-	public void Creatavola() {
-		
+	public static Object[][] tavolaDaGioco = new Object[9][9];
+	
+	public static void tavola() {
 		tavolaDaGioco[1][1] = new Tessera(null);
 		tavolaDaGioco[1][2] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[1][3] = new Tessera(Colore.VERDE);
@@ -37,20 +37,10 @@ public class Tavola {
 		tavolaDaGioco[3][8] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[3][9] = new Tessera(Colore.VERDE);
 		
-		tavolaDaGioco[3][1] = new Tessera(null);
-		tavolaDaGioco[3][2] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[3][3] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[3][4] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[3][5] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[3][6] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[3][7] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[3][8] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[3][9] = new Tessera(Colore.VERDE);
-		
 		tavolaDaGioco[4][1] = new Tessera(null);
 		tavolaDaGioco[4][2] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[4][3] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[2][4] = new Tessera(Colore.VERDE);
+		tavolaDaGioco[4][4] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[4][5] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[4][6] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[4][7] = new Tessera(Colore.VERDE);
@@ -58,11 +48,11 @@ public class Tavola {
 		tavolaDaGioco[4][9] = new Tessera(Colore.VERDE);
 		
 		tavolaDaGioco[5][1] = new Tessera(null);
-		tavolaDaGioco[5][2] = new Tessera(Colore.VERDE);
+		tavolaDaGioco[5][2] = new Tessera(Colore.CIANO);
 		tavolaDaGioco[5][3] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[5][4] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[5][5] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[5][6] = new Tessera(Colore.VERDE);
+		tavolaDaGioco[5][5] = new Tessera(Colore.BIANCO);
+		tavolaDaGioco[5][6] = new Tessera(Colore.CIANO);
 		tavolaDaGioco[5][7] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[5][8] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[5][9] = new Tessera(Colore.VERDE);
@@ -78,7 +68,7 @@ public class Tavola {
 		tavolaDaGioco[6][9] = new Tessera(Colore.VERDE);
 		
 		tavolaDaGioco[7][1] = new Tessera(null);
-		tavolaDaGioco[7][2] = new Tessera(Colore.VERDE);
+		tavolaDaGioco[7][2] = new Tessera(Colore.CIANO);
 		tavolaDaGioco[7][3] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[7][4] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[7][5] = new Tessera(Colore.VERDE);
@@ -90,22 +80,22 @@ public class Tavola {
 		tavolaDaGioco[8][1] = new Tessera(null);
 		tavolaDaGioco[8][2] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[8][3] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[8][4] = new Tessera(Colore.VERDE);
+		tavolaDaGioco[8][4] = new Tessera(Colore.CIANO);
 		tavolaDaGioco[8][5] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[8][6] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[8][7] = new Tessera(Colore.VERDE);
+		tavolaDaGioco[8][7] = new Tessera(Colore.CIANO);
 		tavolaDaGioco[8][8] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[8][9] = new Tessera(Colore.VERDE);
 		
 		tavolaDaGioco[9][1] = new Tessera(null);
-		tavolaDaGioco[9][2] = new Tessera(Colore.VERDE);
+		tavolaDaGioco[9][2] = new Tessera(Colore.CIANO);
 		tavolaDaGioco[9][3] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[9][4] = new Tessera(Colore.VERDE);
 		tavolaDaGioco[9][5] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[9][6] = new Tessera(Colore.VERDE);
+		tavolaDaGioco[9][6] = new Tessera(Colore.CIANO);
 		tavolaDaGioco[9][7] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[9][8] = new Tessera(Colore.VERDE);
-		tavolaDaGioco[9][9] = new Tessera(Colore.VERDE);
-		
+		tavolaDaGioco[9][8] = new Tessera(Colore.CIANO);
 	}
+	
 }
+
