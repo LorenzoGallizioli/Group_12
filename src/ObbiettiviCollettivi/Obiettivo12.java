9923e569e486615ea42a80b88ccbcf870ecc9c7b
LorenzoGallizioli <l.gallizioli1@studenti.unibg.it>
2023-05-31 11:21:33 +0200

    implements obiettivo 12

--- src/ObbiettiviCollettivi/Obiettivo12.java
+++ src/ObbiettiviCollettivi/Obiettivo12.java
@@ -12,19 +12,45 @@ import Game.Tessera;
 public class Obiettivo12 extends ObiettivoComune {
 	
 	public boolean check(Tessera[][] libreria) {
-		int start = 0;
+
+		int counter = 0;
+		int rig = 0;
 		
-		for(int col = 0; col < 4; col++) {
-			if(libreria[0][col].getColor() == Color.BLACK) {
+		// Decrescente.
+		for(int col = 0; col < 5; col++) {
+			
+			if(libreria[rig][col].getColor() != Color.BLACK) {
 				return false;
+			}				
+			
+			if(libreria[rig][col].getColor() == Color.BLACK && libreria[rig + 1][col].getColor() != Color.BLACK) {
+				counter++;					
+			}
+			rig++;
+			
+			if(counter == 5) {
+				return true;
 			}
 		}
 		
-		for(int rig = start; rig < 5; rig++) {
-			if(rig == 5) {
-				start++;
+		// Crescente.
+		counter = 0;
+		rig = 4;
+		for(int col = 0; col < 5; col++) {
+			if(libreria[rig][col].getColor() != Color.BLACK) {
+				return false;
+			}				
+			
+			if(libreria[rig][col].getColor() == Color.BLACK && libreria[rig + 1][col].getColor() != Color.BLACK) {
+				counter++;					
+			}
+			rig--;
+			
+			if(counter == 5) {
+				return true;
 			}
 		}
+		return false;
 	}
 
 }
